f734e04ae4e485990c22cf2c79dfbc823d8830cb
KishenSharma6 <57292990+KishenSharma6@users.noreply.github.com>
2022-03-13 14:24:19 -0700

    Test ETL class functionality

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,17 @@
+#include "../headers/etl.h"
+
 #include <iostream>
+#include <string>
+#include </opt/homebrew/Cellar/eigen/3.4.0_1/include/eigen3/Eigen/Dense>
+#include </opt/homebrew/Cellar/boost/1.78.0_1/include/boost/algorithm/string.hpp>
+#include <vector>
 
 
-int main ()
+int main(int argc, char *argv[])
 {
-    return 0;
+    ETL etl(argv[1], argv[2], argv[3]);
+
+    std::vector<std::vector<std::string> > dataset = etl.readCSV();
+
+    return EXIT_SUCCESS;
 }
