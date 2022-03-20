48c01a8d3586f33aa16751949ed34530d721bd33
KishenSharma6 <57292990+KishenSharma6@users.noreply.github.com>
2022-03-20 14:14:16 -0700

    Added OLS and gradient descent

---
+++ headers/linearRegression.h
@@ -0,0 +1,17 @@
+#ifndef linearRegression_h
+#define linearRegression_h
+
+#include </opt/homebrew/include/eigen3/Eigen/Dense>
+
+class LinearRegression
+{
+public:
+    LinearRegression(){} // constructor
+
+    float OLS_Cost(Eigen::MatrixXd X, Eigen::MatrixXd y, Eigen::MatrixXd theta);
+    std::tuple<Eigen::VectorXd, std::vector<float>> GradientDescent(Eigen::MatrixXd X, Eigen::MatrixXd y, Eigen::VectorXd theta, float alpha, int iters);
+
+
+};
+
+#endif
